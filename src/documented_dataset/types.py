8837ada0a8a34e13ff944ced0b1c680c5412a697
battisti456 <battisti456@gmail.com>
2026-08-07 13:23:39 -0700

    finished fixing typing

--- src/documented_dataset/types.py
+++ src/documented_dataset/types.py
@@ -7,7 +7,7 @@ import xarray as xr
 if TYPE_CHECKING:
     from .documented_dataset import Documented_Dataset
 
-DocumentedDatasetType = TypeVar("DocumentedDatasetType", bound='Documented_Dataset')
-DocumentedBulkFunction = Callable[[type[DocumentedDatasetType]],xr.DataArray|dict[str,xr.DataArray]]
-DocumentedFunction = Callable[[type[DocumentedDatasetType]],xr.DataArray]
 type AttributeName = str|StrEnum
+DocumentedDatasetType = TypeVar("DocumentedDatasetType", bound='Documented_Dataset')
+DocumentedBulkFunction = Callable[[type[DocumentedDatasetType]],xr.DataArray|dict[AttributeName,xr.DataArray]]
+DocumentedFunction = Callable[[type[DocumentedDatasetType]],xr.DataArray]
