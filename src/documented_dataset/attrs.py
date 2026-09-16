78ef3b42df2720379588fbcb3f1df111f463939d
battisti456 <battisti456@gmail.com>
2026-09-16 09:03:26 -0700

    added short_name to attrs

--- src/documented_dataset/attrs.py
+++ src/documented_dataset/attrs.py
@@ -4,6 +4,7 @@ import xarray as xr
 class Attrs(TypedDict, total = False):
     units:str
     description:str
+    short_name:str
     long_name:str
     symbol:str
     fill_value:Any|None
@@ -14,6 +15,8 @@ def repr(arr:xr.DataArray) -> str|None:
     attrs = arr.attrs
     if "symbol" in attrs:
         return attrs["symbol"]
+    if "short_name" in attrs:
+        return attrs["short_name"]
     if "long_name" in attrs:
         return attrs["long_name"]
     return arr.name#type:ignore
