cb4d4f428d65a613f7bdc85fcbbcada926c88b10
battisti456 <battisti456@gmail.com>
2026-09-16 08:56:53 -0700

    added a symbol option to attrs

--- src/documented_dataset/attrs.py
+++ src/documented_dataset/attrs.py
@@ -1,11 +1,19 @@
 from typing import Any, TypedDict, Unpack
-
+import xarray as xr
 
 class Attrs(TypedDict, total = False):
     units:str
     description:str
     long_name:str
+    symbol:str
     fill_value:Any|None
 class attrs(dict):
     def __init__(self,**kwargs: Unpack[Attrs]):
         super().__init__(**kwargs)
+def repr(arr:xr.DataArray) -> str|None:
+    attrs = arr.attrs
+    if "symbol" in attrs:
+        return attrs["symbol"]
+    if "long_name" in attrs:
+        return attrs["long_name"]
+    return arr.name#type:ignore
